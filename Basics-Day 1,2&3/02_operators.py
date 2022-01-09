ec91c3115ef4f770d2111914a8369e29bc95388c
Abdul Haseeb <haseebkhalilshaikh@gmail.com>
2022-01-09 15:42:20 +0500

    my first commit

---
+++ Basics-Day 1,2&3/02_operators.py
@@ -0,0 +1,10 @@
+print(2+3)
+print(2*3)
+print(6/3)
+print(6//3)
+print(6**3)
+print(7%3)
+# Python follows PEMDAS rule that is abbreviation of 
+# Parenthesis, Exponential, Multiply, Division, Addition, Substraction
+# It follows from left to right M D and A S
+print(2**2*2//8+1-2)
